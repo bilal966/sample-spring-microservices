f195eb22fe1ff974d03cca1e0bc5733b67d7a775
M Bilal Ilyas <bilal.hobnail@gmail.com>
2025-03-06 14:36:50 +0000

    init commit

---
+++ account-service/src/test/java/pl/piomin/microservices/account/api/ApiTest.java
@@ -0,0 +1,42 @@
+ /**
+ * 
+ */
+package pl.piomin.microservices.account.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author mubi
+ *
+ */
+@Disabled
+class ApiTest {
+
+	/**
+	 * Test method for {@link pl.piomin.microservices.account.api.Api#findByNumber(java.lang.String)}.
+	 */
+	@Test
+	void testFindByNumber() { 
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pl.piomin.microservices.account.api.Api#findByCustomer(java.lang.Integer)}.
+	 */
+	@Test
+	void testFindByCustomer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pl.piomin.microservices.account.api.Api#findAll()}.
+	 */
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+}
